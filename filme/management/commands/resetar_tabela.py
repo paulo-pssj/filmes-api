dccfca8291840daaf4118e7450c7ce0827e15040
paulo-pssj <paulinhops99@gmail.com>
2021-12-23 11:40:20 -0300

    add comando de deletar tabela

--- filme/management/commands/resetar_tabela.py
+++ filme/management/commands/resetar_tabela.py
@@ -4,9 +4,12 @@ from filme.models import Filme
 class Command(BaseCommand):
     help = 'Deleta todos dados da tabala filme'
     
-    def handle(self):
-        try:
+    def add_arguments(self, parser):
+        parser.add_argument('--drop-all', action='store_true', help=help)
+        
+    def handle(self, *args, **options):
+        if options.get("drop_all"):
+            self.warn("Apangando registros")
             Filme.objects.all().delete()
         
-        except:
-            raise CommandError('Erro ao deletar tabela')
+        
